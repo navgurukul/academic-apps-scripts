43148d9fde767e0be4d5739509dec911bb19934d
ck-surya <chhotu22@navgurukul.org>
2024-01-08 22:21:16 +0530

    extra condtion removed for the checking same thing on username validation library.

--- user_name_validate_library/Code.js
+++ user_name_validate_library/Code.js
@@ -1,52 +1,43 @@
-function validateUserName(userName, site) {
-  if (site == "CC") {
-    return validateCCUsername(userName);
-  }
-  if (site == "AC") {
-    return validateACUsername(userName);
-  }
-  if (site == "LC") {
-    return validateLCUsername(userName);
-  }
-  if (site == "FCC") {
-    return validateFCCUsername(userName);
-  }
-}
-
 function validateCCUsername(userName) {
-  res = checkUserName("https://www.codechef.com/users/"+userName);
-  if (res.getResponseCode() === 200 && res.getContentText().indexOf('"currentUser":"' + userName+ '"') !== -1) {
+  res = checkUserName("https://www.codechef.com/users/" + userName);
+  if (res.getResponseCode() === 200 && res.getContentText().indexOf('"currentUser":"' + userName + '"') !== -1) {
     return "Valid";
   } else {
     return "Invalid";
   }
 }
 
-function validateACUsername(userName){
-  return checkUserName("https://leetcode.com/"+userName).getResponseCode()!==404?"Valid":"Invalid";
+function validateLCUsername(userName) {
+  return checkUserName("https://leetcode.com/" + userName).getResponseCode() !== 404 ? "Valid" : "Invalid";
 }
 
-function validateLCUsername(userName){
-  return checkUserName("https://atcoder.jp/users/"+userName).getResponseCode()!==404?"Valid":"Invalid";
+function validateACUsername(userName) {
+  return checkUserName("https://atcoder.jp/users/" + userName).getResponseCode() !== 404 ? "Valid" : "Invalid";
 }
 
-function validateFCCUsername(userName){
-  return checkUserName("https://www.freecodecamp.org/"+userName).getResponseCode()!==404?"Valid":"Invalid";
+function validateFCCUsername(userName) {
+  return checkUserName('https://api.freecodecamp.org/api/users/get-public-profile?username=' + userName).getResponseCode() !== 404 ? "Valid" : "Invalid";
 }
 
-function checkUserName(url){
+function checkUserName(url) {
   var options = {
     "method": "GET",
     'headers': { 'User-Agent': 'PostmanRuntime/7.32.2' },
     "muteHttpExceptions": true
   };
-  return fetchHTTPResponse(url,options);
+  return fetchHTTPResponse(url, options);
 }
 
 function fetchHTTPResponse(url, options) {
   Logger.log("In fetchHTTPResponse: Fetching url - %s", url);
-  //Logger.log(options);
   var response = UrlFetchApp.fetch(url, options);
   Logger.log(response.getResponseCode());
   return response;
+}
+
+
+function test() {
+  var user = "cksurya";
+
+  Logger.log(validateACUsername(user));
 }
